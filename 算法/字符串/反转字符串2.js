368d49f9ea15bd31c19ff33bc7cd201236dda782
ullielEverGreen <ulliel_luo1997@163.com>
2025-10-19 22:30:26 +0800

    feat：反转字符串||。

--- 算法/字符串/反转字符串2.js
+++ 算法/字符串/反转字符串2.js
@@ -1,47 +1,33 @@
-// 给定一个字符串 s 和一个整数 k，从字符串开头算起，每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
+// 给定一个字符串 arr 和一个整数 k，从字符串开头算起，每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
 
 // 如果剩余字符少于 k 个，则将剩余字符全部反转。
 // 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
 
 /**
- * @param {string} s
+ * @param {string} string
  * @param {number} k
  * @return {string}
  */
 var reverseStr = function(s, k) {
+  const arr = s.split('')
 
-  let startIdx = 0
+  for (let i = 0; i < arr.length; i += k*2) {
+    let left = i
+    // 以arr.length - 1做兜底，避免剩余字符少于k个情况下的越界问题
+    // 比如 s = 'a', k = 2
+    let right = Math.min(i + k - 1, arr.length - 1)
 
-  for (let i = 0; i < s.length; i++) {
-    if ((s.length - i) > k*2) {
-      if ((i+1) % (k*2) === 0) {
-        for (let j = startIdx; j < startIdx + k; j++) {
-          console.log(startIdx + k*2 - j - 1)
-          const temp = s[j]
-          s[j] = s[startIdx + k*2 - j]
-          s[startIdx + k*2 - i] = temp
-        }
-        startIdx += k*2
-      }
-    } else {
-      if (s.length - i > k) {
-        for (let j = startIdx; j < startIdx + k; j++) {
-          const temp = s[j]
-          s[j] = s[startIdx + k*2 - j]
-          s[startIdx + k*2 - i] = temp
-        }
-      } else {
-        for (let j = startIdx; i < s.length; j++) {
-          const temp = s[j]
-          s[j] = s[s.length - j - startIdx]
-          s[s.length - j - startIdx] = temp
-        }
-      }
+    while(left < right) {
+      [arr[left], arr[right]] = [arr[right], arr[left]]
+      left++
+      right--
     }
   }
 
-  return s
+  return arr.join('')
 };
+
+
 
 const s = "abcdefg", k = 2
 const res = reverseStr(s, k)
