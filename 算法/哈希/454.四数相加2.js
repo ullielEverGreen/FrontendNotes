495a0ae76b702895126402049014149d4199b3f5
ullielEverGreen <ulliel_luo1997@163.com>
2025-10-21 20:57:53 +0800

    feat：四数相加。

--- 算法/哈希/454.四数相加2.js
+++ 算法/哈希/454.四数相加2.js
@@ -12,5 +12,35 @@
  * @return {number}
  */
 var fourSumCount = function(nums1, nums2, nums3, nums4) {
-    
+  const map = new Map()
+  let k = 0
+
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      const sum = nums1[i] + nums2[j]
+      map.set(sum, (map.get(sum) || 0) + 1)
+    }
+  }
+
+  for (let i = 0; i < nums3.length; i++) {
+    for (let j = 0; j < nums4.length; j++) {
+      const sum = nums3[i] + nums4[j]
+      const target = 0 - sum
+      if (map.has(target)) {
+        k += map.get(target)
+      }
+    }
+  }
+  return k
 };
+
+// (map.get(num) || 0) + 1
+// 等价于
+// if (map.has(sum)) {
+//     map.set(sum, map.get(sum) + 1);
+// } else {
+//     map.set(sum, 1);
+// }
+
+const nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+console.log(fourSumCount(nums1, nums2, nums3, nums4))
