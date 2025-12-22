df6198e091548e7a9094db1720b0d3b03dbe34b7
ullielEverGreen <ulliel_luo1997@163.com>
2025-12-22 16:35:16 +0800

    perf。

--- test.js
+++ test.js
@@ -22,3 +22,16 @@ const isHappy = (n) => {
 
 const n = 2
 console.log(isHappy(n))
+const dummy = new ListNode(0)
+dummy.next = head
+let current = dummy
+
+while(current !== null) {
+  if (current.next.val === val) {
+    current.next = current.next.next
+  } else {
+    current = current.next
+  }
+}
+
+return dummy.next
